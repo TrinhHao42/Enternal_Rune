93d3601be360efba5a2a7ec17a4033fa28d85292
Khang Lê <duykhang12a7@gmail.com>
2025-10-18 18:06:47 +0700

    feat(warranty): add WarrantyAndReturnPolicyScreen component

---
+++ enternal-rune-user-frontend/src/app/WarrantyAndReturnPolicy/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WarrantyAndReturnPolicyScreen = () => {
+  return (
+    <div><WarrantyAndReturnPolicyScreen /></div>
+  )
+}
+
+export default WarrantyAndReturnPolicyScreen
