ed4e51c8730eb7b0665405c92edb1e1644f91bf2
Trịnh Quang Toàn Hảo <142070040+TrinhHao42@users.noreply.github.com>
2025-10-18 17:21:13 +0700

    create project

---
+++ enternal-rune-backend/src/main/java/iuh/fit/se/enternalrunebackend/EnternalRuneBackendApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.enternalrunebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnternalRuneBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnternalRuneBackendApplication.class, args);
+    }
+
+}
