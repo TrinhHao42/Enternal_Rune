e7c2fa3af3bd680e3f15f3e2b6f45586d26d9df9
Trịnh Quang Toàn Hảo <142070040+TrinhHao42@users.noreply.github.com>
2025-10-20 23:18:12 +0700

    Toan hao
    update entity

---
+++ enternal-rune-user-frontend/src/app/PaymentScreen/page.tsx
@@ -0,0 +1,12 @@
+import Payment from '@/pages/Payment'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex-1'>
+      <Payment />
+    </div>
+  )
+}
+
+export default page
