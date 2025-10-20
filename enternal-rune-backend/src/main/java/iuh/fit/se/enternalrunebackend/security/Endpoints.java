ee0db4eacba70c45d3380d7908f074e999fdc54e
buikhanhlan1410 <buikhanhlan1410@gmail.com>
2025-10-20 15:19:11 +0700

    update

--- enternal-rune-backend/src/main/java/iuh/fit/se/enternalrunebackend/security/Endpoints.java
+++ enternal-rune-backend/src/main/java/iuh/fit/se/enternalrunebackend/security/Endpoints.java
@@ -3,11 +3,15 @@ package iuh.fit.se.enternalrunebackend.security;
 public class Endpoints {
     public static final String front_end_host = "http://localhost:3000";
     public static final String[] PUBLIC_GET_ENDPOINTS = {
-            "/brands/**"
+            "/brands/**",
+            "/products/**",
+            "/categories/**",
+            "/addresses/**",
     };
     public static final String[] PUBLIC_POST_ENDPOINTS = {
             "/account/register",
             "/account/login",
+            "/addresses/**",
     };
     public static final String[] ADMIN_GET_ENDPOINTS = {
 
